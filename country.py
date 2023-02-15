8aa636a3bd1680e4c40c9cd7e5101a3dde827590
Hanan-Hussein <hanhuss035@gmail.com>
2023-02-15 17:28:40 +0300

    id and version added ID and version

--- country.py
+++ country.py
@@ -2,6 +2,8 @@
 from xml.dom import minidom
 import os
 import requests
+import uuid
+import json
 
 
 def GenerateXML(country):
@@ -11,18 +13,21 @@ def GenerateXML(country):
     insert.setAttribute('tableName','COUNTRY')
 
     xml.appendChild(insert)
+  
 
     for key, value in x.items():
+        # print(key,">>>",value)
         columns = root.createElement('column')
-        columns.setAttribute('name', key)
+        columns.setAttribute('name', key.upper())
         columns.setAttribute('value', value)
+       
 
         insert.appendChild(columns)
 
 
     xml_str = root.toprettyxml(indent ="\t") 
     
-    save_path_file = "gfg.xml"
+    save_path_file = "country.xml"
     
     with open(save_path_file, "w") as f:
         f.write(xml_str) 
@@ -40,13 +45,25 @@ x = requests.get("https://countriesnow.space/api/v0.1/countries/flag/unicode")
 allCountries = x.json()["data"]
 
 
-items = [{"name":"ID", "value":"2490d995-9e66-8105-fbd1-9c00cd1c8b7B"},
- {"name":"VERSION", "value":"1"},{"name":"ISO3",'value':'WKN'},{"name":"NAME","value":"WAKANDA"},
- {"name":"UNICODE_FLAG", "value":"🇧🇫"},{"name":"ISO","value":"BF"}
- ]
-thisKeys= set()
+# items = [{"name":"ID", "value":"2490d995-9e66-8105-fbd1-9c00cd1c8b7B"},
+#  {"name":"VERSION", "value":"1"},{"name":"ISO3",'value':'WKN'},{"name":"NAME","value":"WAKANDA"},
+#  {"name":"UNICODE_FLAG", "value":"🇧🇫"},{"name":"ISO","value":"BF"}
+#  ]
+# thisKeys= set()    
+
+# allCountries.append({"name":"ID","value":myuuidStr})
+# allCountries.append({"name":"value", "VERSION":"1"})
+
 
 for x in allCountries:
+    
+    myuuid = uuid.uuid4()
+    myuuidStr = str(myuuid)
+    
+    x.update({"id":myuuidStr})
+    print(x)
+    x.update({"version":"1"})
+  
     GenerateXML(x)
 
     # for key, value in x.items():
