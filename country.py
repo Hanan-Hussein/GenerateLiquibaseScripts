b49ffae3f5ffe79364a97987606982c6f59f1691
Hanan-Hussein <hanhuss035@gmail.com>
2023-02-15 15:47:48 +0300

    ft:xml-generate liquibase  xml file

---
+++ country.py
@@ -0,0 +1,55 @@
+
+from xml.dom import minidom
+import os
+import requests
+
+
+def GenerateXML(name, value):
+
+    insert = root.createElement('insert') 
+    insert.setAttribute('dbms','postgresql, mssql, hsqldb')
+    insert.setAttribute('tableName','COUNTRY')
+
+    xml.appendChild(insert)
+
+    columns = root.createElement('column')
+    columns.setAttribute('name', name)
+    columns.setAttribute('value', value)
+
+    insert.appendChild(columns)
+
+
+    xml_str = root.toprettyxml(indent ="\t") 
+    
+    save_path_file = "gfg.xml"
+    
+    with open(save_path_file, "w") as f:
+        f.write(xml_str) 
+
+root = minidom.Document()
+
+xml = root.createElement('changeSet') 
+xml.setAttribute('author','Tathmini')
+xml.setAttribute('context','!cuba')
+
+root.appendChild(xml)
+
+x = requests.get("https://countriesnow.space/api/v0.1/countries/flag/unicode")
+
+allCountries = x.json()["data"]
+
+
+items = [{"name":"ID", "value":"2490d995-9e66-8105-fbd1-9c00cd1c8b7B"},
+ {"name":"VERSION", "value":"1"},{"name":"ISO3",'value':'WKN'},{"name":"NAME","value":"WAKANDA"},
+ {"name":"UNICODE_FLAG", "value":"🇧🇫"},{"name":"ISO","value":"BF"}
+ ]
+thisKeys= set()
+
+for x in allCountries:
+    # GenerateXML(x["name"], x["value"])
+
+    for key, value in x.items():
+        # print(x[key])
+        GenerateXML(key,value)
+# for i in items:
+#     print(i)
